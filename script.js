39059a1f7c8f7ffda1858deaf9703f31e944d6ca
Atharva <atharvakabadi588@gmail.com>
2023-07-11 00:05:46 +0530

    demand

---
+++ script.js
@@ -0,0 +1,51 @@
+function save(){
+    var key = document.getElementById('note-title').value;
+    let a = document.getElementById("note-field").value;
+    localStorage.setItem(key, a);
+    
+    let html = '';
+    for (let [a, b] of Object.entries(localStorage)) {
+        
+        html += `<p class="display_key">${a} </p> <p class="display_value"> ${b} </p>`;
+       
+      }
+      document.getElementById('note-display').innerHTML = html;
+    
+
+    document.getElementById('note-title').value = '';
+    document.getElementById("note-field").value = '';
+}
+
+let html = '';
+for (let [a, b] of Object.entries(localStorage)) {
+    
+    html += `<p class="display_key">${a} </p> <p class="display_value"> ${b} </p>`;
+   
+  }
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  document.getElementById('note-display').innerHTML = html;
+  
+
+
+  const deleteBtn = document.getElementById('deleteBtn');
+  deleteBtn.addEventListener('click', () => {
+    localStorage.clear();
+    document.getElementById('note-display').innerHTML = '';
+  });
+  
